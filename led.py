75460f1fdb8036845785db1790ddf42c9d520a2a
Kapil Bhatt <101167935+kapilbhatt99@users.noreply.github.com>
2022-03-30 22:15:00 +0530

    samplecommit

---
+++ led.py
@@ -0,0 +1,17 @@
+from gpiozero import LED
+from time import sleep
+import time
+import pyrebase
+#from firebasedata import LiveData
+class LED_code:
+    config = {
+    "apiKey": "AIzaSyBqH-1SFbLkCjaW_Qilj7TQq-2E3cy43FQ",
+    "authDomain": "iot-project-e0803.firebaseapp.com",
+    "databaseURL": "https://iot-project-e0803-default-rtdb.firebaseio.com/",
+    "projectId": "iot-project-e0803",
+    "storageBucket": "iot-project-e0803.appspot.com",
+    "messagingSenderId": "292898203599",
+    "appId": "1:292898203599:web:382bc1a3eb5c9e28c4d4ba",
+    "measurementId": "G-FY4R61HR0P"
+  	};
+    app = pyrebase.initialize_app(config)
